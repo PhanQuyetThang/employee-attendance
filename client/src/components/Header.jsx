cc891a9de50cbe2e3e9cacf16ccb02fb025ddc31
Quyet Thang <quyetthangmarvel@gmail.com>
2023-11-04 12:31:28 +0700

    init git repo

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,44 @@
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+export default function Header() {
+    const { currentUser } = useSelector(state => state.user)
+    return (
+        <header className='bg-slate-200 shadow-md'>
+            <div className='flex justify-between items-center max-w-6xl p-3'>
+                <Link to='/'>
+                    <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                        <span className='text-slate-500'> Attendace </span>
+                        <span className='text-slate-700'> System </span>
+                    </h1>
+                </Link>
+                <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+                    <input type="text" placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' />
+                    <FaSearch className='text-slate-500' />
+                </form>
+                <ul className='flex gap-4'>
+                    <Link to='/'>
+                        <li className='hidden sm:inline text-slate-500 hover:underline'>Home</li>
+                    </Link>
+                    <Link to='/about'>
+                        <li className='hidden sm:inline text-slate-500 hover:underline'>About</li>
+                    </Link>
+                    <Link to='/manage'>
+                        {currentUser && currentUser.role == 'admin' && (
+                            <li className='hidden sm:inline text-slate-500 hover:underline'>Manage</li>
+                        )}
+
+                    </Link>
+                    <Link to='/profile'>
+                        {currentUser ? (
+                            <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="profile" />
+                        ) : (
+                            <li className=' text-slate-500 hover:underline'>Sign In</li>
+                        )}
+                    </Link>
+                </ul>
+            </div>
+        </header >
+    )
+}
---
+++ client/src/pages/Manage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Manage() {
+    return (
+        <div>
+            <h1 className='text-slate-500 text-xl text-center font-semibold mt-5'>Only Admin can access this page!</h1>
+        </div>
+    )
+}
